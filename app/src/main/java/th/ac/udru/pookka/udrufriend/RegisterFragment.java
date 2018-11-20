7da4785f4583d5fa6aa33a9574fe43b0c2013d59
p00kka <sawitree@udru.ac.th>
2018-11-20 14:28:12 +0700

    20/11 check form input

--- app/src/main/java/th/ac/udru/pookka/udrufriend/RegisterFragment.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/RegisterFragment.java
@@ -10,8 +10,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -25,6 +29,7 @@ public class RegisterFragment extends Fragment {
 
     private Uri uri;
     private ImageView imageView;
+    private boolean aBoolean = true;
 
 
     @Override
@@ -40,17 +45,89 @@ public class RegisterFragment extends Fragment {
     }   // main Method
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            checkData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkData() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+//        Get value from edit text to getString()
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtMail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+        EditText repasswordEditText = getView().findViewById(R.id.edtRePassword);
+        EditText telEditText = getView().findViewById(R.id.edtTel);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String emailString = emailEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+        String repasswordString = repasswordEditText.getText().toString().trim();
+        String telString = telEditText.getText().toString().trim();
+
+
+        if (aBoolean) {
+            myAlert.normalDialog("No Avatar", "Please choose image");
+        } else if (checkSpace(nameString, emailString, passwordString, repasswordString, telString)) {
+            myAlert.normalDialog(getString(R.string.title_have_space), getString(R.string.message_have_space));
+        } else if (passwordString.equals(repasswordString)) {
+
+            uploadtoFirebase(nameString, emailString, passwordString,telString);
+        } else {
+            myAlert.normalDialog("Password not match", "Please type same password");
+
+        }
+
+
+    }
+
+    private void uploadtoFirebase(String nameString, String emailString, String passwordString, String telString) {
+
+    }
+
+    private boolean checkSpace(String nameString,
+                               String emailString,
+                               String passwordString,
+                               String repasswordString,
+                               String telString) {
+        boolean result = false;
+        if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty() || repasswordString.isEmpty() || telString.isEmpty()) {
+            result = true;
+
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 //        resultcode is either true or false
-        if (resultCode == getActivity().RESULT_OK  ) {
+        if (resultCode == getActivity().RESULT_OK) {
 
             uri = data.getData();
             try {
                 Bitmap bitmap = BitmapFactory.decodeStream(getActivity()
-                                .getContentResolver()
-                                .openInputStream(uri)); //draw image, large, used up lots of memories
-                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800,600,false);
+                        .getContentResolver()
+                        .openInputStream(uri)); //draw image, large, used up lots of memories
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
                 //scale image into another image
                 imageView.setImageBitmap(bitmap1);
 
@@ -73,14 +150,15 @@ public class RegisterFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "Please select 5 pics"),5);
+                startActivityForResult(Intent.createChooser(intent, "Please select 5 pics"), 5);
             }
         });
     } // open app to choose pic
 
     private void createToolbar() {
+
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.title_have_space);
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
@@ -91,6 +169,8 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
     }
 
     public RegisterFragment() {
