a9c648ce3c14f1bf24a8f6cf2a09d8e275be0222
p00kka <sawitree@udru.ac.th>
2018-11-21 14:35:47 +0700

    21/11 sign in + authen

--- app/src/main/java/th/ac/udru/pookka/udrufriend/MainFragment.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/MainFragment.java
@@ -3,13 +3,19 @@ package th.ac.udru.pookka.udrufriend;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -23,14 +29,10 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
 //        check login status
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        if (firebaseAuth.getCurrentUser() != null) {
+        checkLoginStatus();
 
-            startActivity(new Intent(getActivity(), ServiceActivity.class));
-            getActivity().finish();
-        }
-
-
+//        login controller
+        loginController();
 
 
 //        Register Controller
@@ -38,6 +40,53 @@ public class MainFragment extends Fragment {
 
 
     }   //main method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtMail = getView().findViewById(R.id.edtMail);
+                EditText edtPassword = getView().findViewById(R.id.edtPassword);
+
+                String emailString = edtMail.getText().toString().trim();
+                String passwordString = edtPassword.getText().toString().trim();
+
+                final MyAlert myAlert = new MyAlert(getActivity());
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                  myAlert.normalDialog(getString(R.string.title_have_space),getString(R.string.message_have_space));
+                } else {
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        movetoService();
+                                    } else {
+
+                                        myAlert.normalDialog("Cannot Sign in", task.getException().toString());
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    private void checkLoginStatus() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+
+            movetoService();
+        }
+    }
+
+    private void movetoService() {
+        startActivity(new Intent(getActivity(), ServiceActivity.class));
+        getActivity().finish();
+    }
 
     private void registerController() {
         //        get value from xml id or initial view
