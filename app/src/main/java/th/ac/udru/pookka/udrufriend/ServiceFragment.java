8a003a374e6ae897b0cc2b21774dbb13724c900b
p00kka <sawitree@udru.ac.th>
2018-11-22 13:48:53 +0700

    22/11 get databasemodel

--- app/src/main/java/th/ac/udru/pookka/udrufriend/ServiceFragment.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/ServiceFragment.java
@@ -2,12 +2,24 @@ package th.ac.udru.pookka.udrufriend;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 /**
@@ -31,7 +43,49 @@ public class ServiceFragment extends Fragment {
 
     private void createRecyclerview() {
         RecyclerView recyclerView = getView().findViewById(R.id.recycleViewFriend);
-    }
+        final String tag = "22noV1";
+
+        final int[] countInts = new int[]{0};
+
+        final ArrayList<String> displayNameArrayList = new ArrayList<>();
+        final ArrayList<String> urlAvatarArrayList = new ArrayList<>();
+
+//        get value from firebase
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("User");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                int i = (int) dataSnapshot.getChildrenCount();
+                Log.d(tag, "number of user =====>" + i);
+
+                ArrayList<DatabaseModel> databaseModelArrayList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    DatabaseModel databaseModel = dataSnapshot1.getValue(DatabaseModel.class);
+                    databaseModelArrayList.add(databaseModel);
+
+                    DatabaseModel databaseModel1 = databaseModelArrayList.get(countInts[0]);
+                    countInts[0] += 1;
+                    displayNameArrayList.add(databaseModel1.getNameString());
+                    urlAvatarArrayList.add(databaseModel1.getPathUrlString());
+                }
+
+                Log.d(tag, "displaynamearraylist ===>" + displayNameArrayList.toString());
+                Log.d(tag, "urlAvatarArraylist ====>" + urlAvatarArrayList.toString());
+
+            } // on data change
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }   // create recyclerview method
 
 
     @Override
